44a340896e82f75c59e72be22c503e6bc387dbbf
Michael Kurze <michael@thefoundation.de>
2013-09-23 23:41:26 +0200

    Steps towards making sense.

--- js/nbe.js
+++ js/nbe.js
@@ -1,11 +1,32 @@
 require(["underscore", "jquery", "jquery.ui"], function(_, $) {
 
-   console.log($("html"));
+   $(".graph .node").draggable( { stack: ".graph *" } )
 
-   console.log(_.map);
 
-   console.log();
+   var $origin;
 
-   $(".node").draggable()
+   function handleDragStart( event, ui ) {
+      console.log( event, ui );
+
+      // if this is an IN port:
+      //    if there is no link here: stop.
+      //    else: $origin = $link.sourcePort
+      // else:
+      //    if there is a $link with
+
+
+      // , draw line from current OUT port to mouse
+
+      // if this is an OUT port:
+      // ... draw line from port to mouse
+
+
+   }
+
+   function updateLink( $link, fromX, fromY, toX, toY ) {
+
+   }
+
+   $(".port i").draggable( { opacity: 0.8, helper: "clone", zIndex: 100000, start: handleDragStart } );
 
 });
